ee70f989f0b2d54473a3e1a014f2bcacf96fa22f
Brian McFarland <mcfardb@dds.dillards.net>
2020-04-30 20:14:56 -0500

    started app and feature routing and using parameters

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -26,12 +27,16 @@ export class ProductListComponent implements OnInit {
   filteredProducts: Product[] = [];
   products: Product[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
       next: products => {
         this.products = products;
+        this.route.queryParamMap.subscribe(params => {
+          this.listFilter = params.get('filterBy') || '';
+          this.showImage = params.get('showImage') === 'true';
+        });
         this.filteredProducts = this.performFilter(this.listFilter);
       },
       error: err => this.errorMessage = err
